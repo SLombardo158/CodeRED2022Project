4e7285eb246bfd35ba2eb2a1e112e372bdc9f926
Anthamorious <shoschool.7@gmail.com>
2022-10-30 00:30:51 -0500

    Battery v.2

---
+++ Battery/battery v2.cpp
@@ -0,0 +1,210 @@
+//
+//  main.cpp
+//  CodeRED Hackathon
+//
+//  Created by Shahad Alghamdi on 10/29/22.
+//
+
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+class Zone{
+public:
+    
+    // Data fields
+    int type; // 1: Charging Stations | 2: Internet Zone | 3: Both
+    double x; // x coordinates
+    double y; // y coordinates
+    
+    // Constructor
+    Zone(int type1, double x1, double y1){
+        type = type1;
+        x = x1;
+        y = y1;
+    }
+    
+    // Functions
+    void setX(double x1){
+        x = x1;
+    }
+    void setY(double y1){
+        y = y1;
+    }
+    void setType(int type1){
+        type = type1;
+    }
+    double getX(){
+        return x;
+    }
+    double getY(){
+        return y;
+    }
+    string getType(){
+        if (type == 1){
+            return "Charging Stations";
+        } else if (type == 2){
+            return "Internet Zone";
+        } else if (type == 3) {
+            return "Both";
+        } else {
+            return "ERROR: Invalid Type";
+        }
+    }
+};
+
+
+class Rover{
+public:
+    // Constructor
+    Rover(double x1, double y1, int state1 = 1){
+        x = x1;
+        y = y1;
+        state = state1;
+        battery = 100.0; // mAh ( just a value for testing )
+    }
+    
+    // setter functions:
+    void setX(double x1){
+        x = x1;
+    }
+    void setY(double y1){
+        y = y1;
+    }
+    void setState(int state1){
+        state = state1;
+    }
+    void setBattery(double battery1){
+        battery = battery1;
+    }
+    
+    // getter functions
+    double getX(){
+        return x;
+    }
+    double getY(){
+        return y;
+    }
+    int getState(){
+        return state;
+    }
+    double getBatteryUsage(){
+        if (state == 1){
+            return 0.0025;
+        } else if (state == 2){
+            return 0.01;
+        } else {
+            return -1; // if the state is not resting or moving (ERROR)
+        }
+    }
+    int getSpeed(){
+        return SPEED;
+    }
+    double getBattery(){
+        return battery;
+    }
+    
+private:
+    // Data fields:
+    double x; // x coordinate
+    double y; // y coordinate
+    int state; // 1: resting | 2: moving
+    const double SPEED = 2.0; // a given speed at 2 m/h
+    double battery; // the battery capacity
+};
+
+
+// Calculate the distance between the rover and an indecated zone
+double getDistance(Zone& zone, Rover& rover){
+    return sqrt(pow(zone.getX() - rover.getX(), 2) + pow(zone.getY() - rover.getY(), 2));
+}
+
+
+// reduces the battery depending on the rover state
+// If moving : -1% per hour
+// if resting : -0.25% per hour
+void reduceBattery(Rover& rover){
+    rover.setBattery(rover.getBattery() - (rover.getBatteryUsage() * rover.getBattery()));
+}
+
+
+// calculates the time taken to reach a certain zone (in hours)
+double destinationTime(Zone& zone, Rover& rover){
+    return getDistance(zone, rover) / rover.getSpeed();
+}
+
+
+// This function evaluates the distance between the rover and the zone indecated, and then calculates the battery used to reach that zone.
+void stateBatteryReduction(Zone& zone, Rover& rover){
+    if (rover.getState() == 2){
+        for (double i = getDistance(zone, rover); i > 0; (i -= 2)){
+            reduceBattery(rover);
+        }
+    } else if (rover.getState() == 1){
+        cout << "The rover is at a resting state." << endl;
+    }
+}
+
+
+// the battery is charged 1% per min
+void chargeBattery(Rover& rover){
+    while (rover.getBattery() <= 100){
+        rover.setBattery(rover.getBattery() + (rover.getBattery() * 0.01));
+    }
+    if (rover.getBattery() > 100){
+        rover.setBattery(100);
+    }
+}
+
+
+int main(){
+    
+    // Welcome to my messy testin zone :D
+    
+    Zone alpha(1, 9, 8);
+    Zone bravo(1, 5, 6);
+    Rover rover(0, 0, 2); // moving
+    Rover rover2(0, 0); // resting
+    
+    cout << "---------------------------------------------------" << endl;
+    
+    cout << "The distance between the rover and Alpha: " << getDistance(alpha, rover) << " miles" << endl;
+    
+    cout << "---------------------------------------------------" << endl;
+    
+    cout << "Moving rover battery: " << rover.getBattery() << " and battery usage: " << rover.getBatteryUsage() << endl;
+    
+    stateBatteryReduction(alpha, rover); // reduces the battery according to the distance taken to reach alpha zone
+    
+    cout << "---------------------------------------------------" << endl;
+    
+    cout << "The battery after reaching alpha: "<< rover.getBattery() << endl;
+    
+    chargeBattery(rover); // well .. charge battery :)
+    
+    cout << "---------------------------------------------------" << endl;
+    
+    cout << "Battery after charging: "<< rover.getBattery() << endl;
+    
+    cout << "---------------------------------------------------" << endl;
+    
+    cout << "Time taken to reach alpha: "<< destinationTime(alpha, rover) << " hours" << endl;
+    
+    cout << "---------------------------------------------------" << endl;
+    
+    // just a simple if statement to check if the distace function works
+    if (getDistance(alpha, rover) >= getDistance(bravo, rover)){
+        cout << "alpha is far" << endl;
+    } else {
+        cout << "nvm" << endl;
+    }
+    cout << "---------------------------------------------------" << endl;
+    return 0;
+}
+
+
+/// Things to polish:
+/// [] A function that reduces the battery at a resting state by using time
+/// [] A function that test whether the rover is at a chargin station or not ( could be a bool )
+/// [] Decide whether to decrease the battery in regards to time or distance
